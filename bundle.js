7884f632c8c0431ffd7dd2295ee77886f1c30b22
Oscar Söderlund <poscar@spotify.com>
2013-09-19 21:03:32 +0200

    Insert cards as global var
    Change-Id: Idef9a1d019072ff1ee820cbf96f258ef806eff9e

--- bundle.js
+++ bundle.js
@@ -4,16 +4,22 @@ var fs = require('fs'),
     cardGenerator = require('./card-generator.js');
 
 var script;
+var cards;
 
 exports.init = function(options) {
   // Write new cards file
   var cedict = fs.readFileSync(path.join(__dirname, 'assets/cedict/cedict_ts.u8'), 'utf-8'),
-      vocabulary = fs.readFileSync(options.vocabularyFile, 'utf-8'),
-      cards = cardGenerator.generateCardsFromCedictAndVocabulary(cedict, vocabulary);
-  fs.writeFileSync(path.join(__dirname, 'cards.js'), 'module.exports = ' + JSON.stringify(cards) + ';');
+      vocabulary = fs.readFileSync(options.vocabularyFile, 'utf-8');
+  cards = cardGenerator.generateCardsFromCedictAndVocabulary(cedict, vocabulary);
   script = browserify(path.join(__dirname, 'lightcards/main.js'));
 };
 
 exports.pipe = function(stream) {
-  script.bundle().pipe(stream);
+  script.bundle({
+    insertGlobalVars: {
+      cards: function() {
+        return JSON.stringify(cards);
+      }
+    }
+  }).pipe(stream);
 };
--- lightcards/main.js
+++ lightcards/main.js
@@ -1,5 +1,4 @@
 var ko = require('knockout'),
-    cards = require('../cards.js'),
     LightCardsViewModel = require('./light-cards-view-model').LightCardsViewModel;
 
 ko.applyBindings(new LightCardsViewModel(cards));
