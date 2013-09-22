6e3e166d9fce3f38f65b1cfa9075f164d2ea4ed2
Oscar Söderlund <poscar@spotify.com>
2013-09-22 19:43:42 +0200

    Move cedict generation into subtask folder
    Change-Id: I3a580bcaf7a12683658def5b70e6f32a6d4488c5

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    cedict: { 'cedict.js': './assets/cedict/cedict_ts.u8' },
+
     compass: {
       dist: {
         options: {
@@ -11,62 +14,18 @@ module.exports = function(grunt) {
         }
       }
     },
+
     watch: {
       styles: {
         files: ['assets/sass/*.sass'],
         tasks: ['compass']
       }
-    },
-    cedict: {
-      'cedict.js': './assets/cedict/cedict_ts.u8'
     }
   });
 
+  grunt.loadTasks('./tasks');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerMultiTask('cedict', 'Parse cedict and generate a JSON data structure.', function() {
-    var parseCedictRow = function(row) {
-      var matches = /^(.+?) (.+?) \[(.+?)\] \/(.+?)\/$/.exec(row.trim());
-      return [matches[2], {
-        transcription: matches[3],
-        translation: matches[4].split('/').filter(function(translation) {
-          return !(translation[0] === 'C' && translation[1] === 'L');
-        }).join(' - ')
-      }];
-    };
-
-    var fs = require('fs'),
-        fileContents = fs.readFileSync(this.data, 'utf-8'),
-        rows = fileContents.trim().split('\n').slice(59),
-        dict = {};
-    for (var i = 0, length = rows.length; i < length; i++) {
-      var row = parseCedictRow(rows[i]),
-          key = row[0],
-          value = row[1];
-      if (dict[key]) {
-        dict[key].alternatives = dict[key].alternatives || [];
-        dict[key].alternatives.push(value);
-      } else {
-        dict[key] = value;
-      }
-    }
-
-    var vocabularyGenerator = function(cedict) {
-      return function(vocabulary) {
-        var _ = require('underscore');
-        vocabulary = vocabulary.trim().split('\n').map(function(line) {
-          return line.trim();
-        });
-        return _.chain(cedict).pick(vocabulary).pairs().map(function(pair) {
-          pair[1].glyph = pair[0];
-          return pair[1];
-        }).value();
-      };
-    };
-
-    fs.writeFileSync(this.target, 'module.exports = (' + vocabularyGenerator.toString() + '(' + JSON.stringify(dict) + '));');
-  });
 
   grunt.registerTask('default', ['compass']);
 };
