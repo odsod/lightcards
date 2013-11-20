f0ed1cce35ee7f55b2751f7f8574ac6b87b20b19
Oscar Söderlund <poscar@spotify.com>
2013-11-20 08:31:37 +0100

    Fix require errors
    Change-Id: I118015fa754e80465246c163ff1d98706cb6caf5

--- bin/init.js
+++ bin/init.js
@@ -4,18 +4,23 @@ var fs = require('fs'),
 var writeFileIfExists = function(file, contents) {
   if (!fs.existsSync(path.join(process.cwd(), file))) {
     fs.writeFileSync(path.join(process.cwd(), file), contents);
-    console.log('File ' + file.cyan + ' created.');
+    console.log('File   ' + file.cyan + ' created.');
   } else {
-    console.log('File ' + file.cyan + ' already exists.');
+    console.log('File   ' + file.cyan + ' already exists.');
   }
 };
 
 module.exports = function() {
-  fs.mkdirSync('lightcards');
-  writeFileIfExists('lightcards/vocabulary.txt',
-                    fs.readFileSync(path.join(__dirname, '../example-vocabulary.txt')));
-  writeFileIfExists('lightcards/config.json',
-                    fs.readFileSync(path.join(__dirname, '../default-config.json')));
-  writeFileIfExists('lightcards/state.json', JSON.stringify([]));
+  if (!fs.existsSync('lightcards')) {
+    console.log('Folder ' + 'lightcards/'.cyan + ' created.');
+    fs.mkdirSync('lightcards');
+    writeFileIfExists('lightcards/vocabulary.txt',
+                      fs.readFileSync(path.join(__dirname, '../example-vocabulary.txt')));
+    writeFileIfExists('lightcards/config.json',
+                      fs.readFileSync(path.join(__dirname, '../default-config.json')));
+    writeFileIfExists('lightcards/state.json', JSON.stringify([]));
+  } else {
+    console.log('Folder ' + 'lightcards/'.cyan + ' already exists.');
+  }
   process.exit(0);
 };
